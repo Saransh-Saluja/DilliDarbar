1be0841e5141b19b41ad6e8e8f7340fb2783a959
ByteStorm05 <143643985+ByteStorm05@users.noreply.github.com>
2025-08-30 21:46:51 +0530

    Menu complete v3

--- src/components/popup.js
+++ src/components/popup.js
@@ -53,7 +53,7 @@ const NotificationPopup = ({
   if (!isVisible) return null;
 
   return (
-    <div className="fixed top-4 right-4 z-50">
+    <div className="fixed top-4 right-4 z-500000">
       <div className={`
         transform transition-all duration-300 ease-out
         ${isAnimating ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0'}
